f3e55905888d75b294165ec6f5cf4a3cef7a7459
Dhruv Kumar <dhruv21@gmail.com>
2016-01-22 16:27:28 -0800

    corrected app name

--- src/main/scala/com/dhruv/Main.scala
+++ src/main/scala/com/dhruv/Main.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.hive.HiveContext
 
 object Main {
   def main(args: Array[String]) {
-    val sparkConf = new SparkConf().setAppName("Twitter Sentiment Analyzer")
+    val sparkConf = new SparkConf().setAppName("Spark to Hive examples")
     val sc = new SparkContext(sparkConf)
     val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
     val df = hiveContext.read.json("file:///usr/hdp/current/spark-client/examples/src/main/resources/people.json")
